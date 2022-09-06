87592cc3bac517d7a6a09719b409fbe639b5cecb
djbpitt <djbpitt@gmail.com>
2022-09-06 17:11:58 -0400

    Planning \(not yet implemented\) checking for next blocks

--- src/test_binary-search.py
+++ src/test_binary-search.py
@@ -8,16 +8,22 @@ from bisect import bisect_right
 test_dict = {1:'a', 4: 'b', 7: 'c', 5: 'd', 10: 'e'}
 
 # Initializing k
-k = 12
+k = 4
+
+# Need to sort keys for bisect operation
+sorted_keys = sorted(test_dict.keys())
 
 # Printing original list
-print("The original list is : " + str(test_dict.keys()))
+print("The input dictionary is " + str(test_dict))
+print("The keys in original order are : " + str(test_dict.keys()))
+print("The sorted keys are : " + str(sorted_keys))
 
 # Using sorted() + bisect_right()
-# to find smallest number
-# greater than k
-sorted_keys = sorted(test_dict.keys())
-min_val = sorted_keys[bisect_right(sorted_keys, k)]
+# to find smallest number greater than k
+# and its position
+index = bisect_right(sorted_keys, k)
+min_val = sorted_keys[index]
 
 # Printing result
-print("The minimum value greater than " + str(k) + " is : " + str(min_val))
+print("The minimum key value greater than " + str(k) + " is : " + str(min_val) + " at index position " + str(index))
+print("The value associated with that key is " + test_dict[min_val])
