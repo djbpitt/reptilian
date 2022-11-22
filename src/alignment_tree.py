a42969e23ab67a4687db369658e68a1368b3e3df
djbpitt <djbpitt@gmail.com>
2022-11-22 16:39:12 -0500

    Processing only leading unaligned tokens
    Leading unaligned tokens are handled specially, but can be
    treated the same way as intermediate unaligned ranges, except
    that start position of ranges is computed differently.

--- src/alignment_tree.py
+++ src/alignment_tree.py
@@ -55,21 +55,36 @@ def expand_node(_graph: nx.DiGraph, _node_ids: deque, _token_array, _token_membe
     _finished = perform_beam_search(_witness_count, _largest_blocks, _block_offsets_by_witness,
                                     _witness_offsets_to_blocks, _score_by_block)
     # Only if current head of queue has blocks
+    # Get information about parent
     _parent_id = _node_ids.popleft()
     _parent = _graph.nodes[_parent_id] # dictionary of properties
-    # _parent = _graph[_node_ids.popleft()] # Parent is node at head of queue
-    # _parent.children = [] # Create list to hold children
-    # _finished[0] is the best path; blocks are in reverse order
+    # Add leading unaligned tokens (if any)
+    # Add new node
+    # Add node as child of parent
+    # Add node to queue
+    _parent_starts = [i[0] for i in _parent["token_ranges"]]
+    _first_block_starts = _largest_blocks[_finished[0].path[-1]][1]
+    if _parent_starts != _first_block_starts:
+        _token_ranges = [(i, j) for i, j in zip(_parent_starts, _first_block_starts)]
+        _id = _graph.number_of_nodes()
+        _graph.add_node(_id, type="potential", token_ranges=_token_ranges, children=[])
+        _parent["children"].append(_id)
+        _node_ids.append(_id)
     # Add blocks as leaf node children (do not add leaf nodes to queue)
+    # TODO: Except for first block, add unaligned tokens as potential node before each block
     for _block_id in _finished[0].path[::-1]:
         # _largest_blocks[block] is a leaf node with shape (26, [4, 12795, 25646, 38708, 52026, 66257])
         # The first value is the length of the block (exclusive)
         # The second is the start positions of the block in each witness, using global token position
+        # If not first block, add potential block first
+        if 
+        # Now add block as aligned leaf node
         _block = _largest_blocks[_block_id]
-        _id = len(_graph.nodes())
+        _id = _graph.number_of_nodes()
         _token_ranges = [(i, i + _block[0]) for i in _block[1]]
         _graph.add_node(_id, type="leaf", token_ranges=_token_ranges, children=[])
         _parent["children"].append(_id)
+    # Add trailing unaligned tokens (if any)
     # Debug report
     for n in _graph.nodes(data=True):
         print(n)
