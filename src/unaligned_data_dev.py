2c9f11d50f7cec113f806df91fd26f4b6bb15401
djbpitt <djbpitt@gmail.com>
2022-12-20 17:45:18 -0500

    Add link to documentation for scikit-learn clustering

--- src/unaligned_data_dev.py
+++ src/unaligned_data_dev.py
@@ -1,2 +1,3 @@
 # https://scikit-learn.org/stable/modules/generated/sklearn.feature_extraction.text.CountVectorizer.html
 # https://stackoverflow.com/questions/35867484/pass-tokens-to-countvectorizer
+# https://www.cse.uoi.gr/~tsap/teaching/cse012/tutorials/Introduction-SciKit-Learn-Clustering.html
