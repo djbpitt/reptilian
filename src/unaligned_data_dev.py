a0672b824de0d4bfda23af7ca8d4cedc0080c8ba
djbpitt <djbpitt@gmail.com>
2022-12-20 17:00:41 -0500

    Experiment with scikit-learn to compare witnesses

---
+++ src/unaligned_data_dev.py
@@ -0,0 +1,2 @@
+# https://scikit-learn.org/stable/modules/generated/sklearn.feature_extraction.text.CountVectorizer.html
+# https://stackoverflow.com/questions/35867484/pass-tokens-to-countvectorizer
